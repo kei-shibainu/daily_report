6b8fa9619c7009ec90fbafa59cf1e212b754b0ed
kei-shibainu <hotaru.personal.works@gmail.com>
2024-10-07 23:42:26 +0900

    pythonに変更

---
+++ twet.py
@@ -0,0 +1,45 @@
+import tweepy
+import os
+import glob
+
+# Twitter APIのキーとトークンを環境変数から取得
+ck = os.environ['X_API_KEY']
+cs = os.environ['X_API_KEY_SECRET']
+bt = os.environ['X_BEARER_TOKEN']
+at = os.environ['X_ACCESS_TOKEN']
+ats = os.environ['X_ACCESS_TOKEN_SECRET']
+
+# Twitter APIのクライアントを初期化
+client = tweepy.Client(
+    bearer_token=bt,
+    consumer_key=ck,
+    consumer_secret=cs,
+    access_token=at,
+    access_token_secret=ats
+)
+
+# 最新の.mdファイルを取得
+def get_latest_markdown_file():
+    # カレントディレクトリ内のすべての.mdファイルを取得
+    markdown_files = glob.glob('*.md')
+    
+    # ファイルが存在しない場合
+    if not markdown_files:
+        return None
+
+    # 最新のファイルを取得
+    latest_file = max(markdown_files, key=os.path.getmtime)
+    return latest_file
+
+# ツイート内容を取得
+latest_md_file = get_latest_markdown_file()
+
+if latest_md_file:
+    with open(latest_md_file, 'r', encoding='utf-8') as file:
+        content = file.read()
+    
+    # ツイートを投稿
+    client.create_tweet(text=content)
+    print('Tweet sent successfully!')
+else:
+    print('No markdown files found.')
