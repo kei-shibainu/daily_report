3f6b5ecd9500bc8c5e5c911943f5a892c44c7cae
kei-shibainu <hotaru.personal.works@gmail.com>
2024-10-07 22:41:34 +0900

    環境変数の取り扱いを修正

--- tweet.js
+++ tweet.js
@@ -3,18 +3,17 @@ const fs = require('fs');
 const path = require('path');
 const { execSync } = require('child_process');
 
-// GitHub SecretsからTwitter APIの情報を取得
 const twitterClient = new TwitterApi({
-  subdomain: "api",
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  appKey: process.env.TWITTER_API_KEY,
+  appSecret: process.env.TWITTER_API_SECRET_KEY,
+  accessToken: process.env.TWITTER_ACCESS_TOKEN,
+  accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
 // 最新のコミットで変更されたMarkdownファイルのパスを取得
 const getLatestMarkdownFilePath = () => {
-  const output = execSync('git diff --name-only HEAD^ HEAD').toString();
+  // コミットの履歴を確認
+  const output = execSync('git diff --name-only HEAD~1 HEAD').toString();
   const files = output.split('\n').filter(Boolean);
   return files.find(file => file.endsWith('.md')); // .mdファイルを探す
 };
